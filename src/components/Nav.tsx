25e58fdd95e58f2affbb16277cd9b9e9f47edff9
Pawel Gronski <p.gronski@mente.io>
2024-12-20 05:49:55 +0100

    Finished landing page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,24 +4,36 @@ import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
-    <header className="padding-x   absolute z-10 w-full py-8">
-      <nav className="max-container flex items-center  justify-between">
+    <header className="padding-x absolute z-10 w-full py-8">
+      <nav className="max-container flex items-center justify-between">
         <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} />
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={129}
+            height={29}
+            className="m-0 h-[29px] w-[129px]"
+          />
         </a>
         <ul className="flex flex-1 items-center justify-center gap-16 max-lg:hidden">
-          {navLinks.map((item) => {
-            return (
-              <li key={item.label}>
-                <a className="font-montserrat text-lg leading-normal text-slate-gray">
-                  {item.label}
-                </a>
-              </li>
-            );
-          })}
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat text-lg leading-normal text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
+        <div className="flex gap-2 font-montserrat text-lg font-medium leading-normal max-lg:hidden wide:mr-24">
+          <a href="/">Sign in</a>
+          <span>/</span>
+          <a href="/">Explore now</a>
+        </div>
         <div className="hidden max-lg:block">
-          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
         </div>
       </nav>
     </header>
